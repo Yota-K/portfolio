f81893c93b2598ce5c2de3da1f5edcfcfcf32806
Y.K <powdersugar828828@gmail.com>
2022-10-08 01:39:41 +0900

    feat: Cording a profile component.

--- src/components/sections/Profile.tsx
+++ src/components/sections/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import SectionRoot from './SectionRoot';
 
 const Profile: React.FC = () => {
@@ -6,9 +7,22 @@ const Profile: React.FC = () => {
     <SectionRoot heading="Profile">
       <div className="lg:mb-10 mb-6">
         <h3 className="font-bold text-xl">Karukichi Soejima</h3>
-        <p>東京都で活動するウェブ開発者です。</p>
-        <p>アイコンのキャラクターの名前が「カルキチ」って言います。</p>
-        <p>彼は、カンガルーとキウィのハーフです。</p>
+        <div className="lg:flex lg:justify-between items-center">
+          <div>
+            <p>東京都で活動するウェブ開発者です。</p>
+            <p>アイコンのキャラクターの名前が「カルキチ」って言います。</p>
+            <p>彼は、カンガルーとキウィのハーフです。</p>
+          </div>
+          <div className="lg:text-start lg:m-0 text-center mt-4 mb-8 ">
+            <StaticImage
+              src="../../images/icon.png"
+              alt="Karukichi"
+              width={100}
+              className="rounded-full border-gray-500 border"
+            />
+            <h4 className="text-sm mt-2">He is Karukichi.</h4>
+          </div>
+        </div>
       </div>
       <div className="lg:mb-10 mb-6">
         <h3 className="font-bold">得意な領域</h3>
--- src/components/sections/SectionRoot.tsx
+++ src/components/sections/SectionRoot.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionRoot: React.FC<{ heading: string; children: React.ReactNode }> = ({ heading, children }) => {
   return (
     <section className="lg:my-6 my-4">
-      <h2 className="lg:mb-4 mb-2  text-3xl font-bold" id={`#${heading.toLowerCase()}`}>
+      <h2 className="lg:mb-4 mb-2 text-3xl font-bold" id={`#${heading.toLowerCase()}`}>
         {heading}
       </h2>
       <div>{children}</div>
