8ddc28962dba6e9b4b29e9d6a3d4f9e31bc24abb
Y.K <powdersugar828828@gmail.com>
2022-10-08 01:04:21 +0900

    feat: Settings meta info.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HeadFC } from 'gatsby';
 import Layout from '~/components/Layout';
 import { Profile, Career, Skills, Others, Contact } from '~/components/sections';
 
@@ -15,3 +16,10 @@ const Top: React.FC = () => {
 };
 
 export default Top;
+
+export const Head: HeadFC = () => (
+  <>
+    <title>Karukichi Soejima Portfolio</title>
+    <meta name="description" content="Karukichi Soejima's portfolio site." />
+  </>
+);
