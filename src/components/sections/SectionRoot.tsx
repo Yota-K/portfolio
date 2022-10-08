a72852f7fc23be18f182cae6d9d2ec2489c7ea1c
Y.K <powdersugar828828@gmail.com>
2022-10-08 18:48:52 +0900

    feat: Added heading css.

--- src/components/sections/SectionRoot.tsx
+++ src/components/sections/SectionRoot.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const SectionRoot: React.FC<{ heading: string; children: React.ReactNode }> = ({ heading, children }) => {
   return (
     <section className="lg:my-6 my-4">
-      <h2 className="lg:mb-4 mb-2 text-3xl font-bold" id={`#${heading.toLowerCase()}`}>
+      <h2
+        className="relative lg:mb-4 mb-2 text-3xl font-bold before:bg-blue-700 before:absolute before:-bottom-1 before:left-0 before:w-10 before:h-1"
+        id={`#${heading.toLowerCase()}`}
+      >
         {heading}
       </h2>
       <div>{children}</div>
