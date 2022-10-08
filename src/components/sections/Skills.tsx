fe6b027b72b13f7a10b2e80316590a201a0aa822
Y.K <powdersugar828828@gmail.com>
2022-10-09 02:20:02 +0900

    feat: Built in Modal component.

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '~/components/Button';
+import Modal from '~/components/Modal';
 import SectionRoot from './SectionRoot';
+import { SkillInfo, allSkills } from '~/allSkills';
 
 const Skills: React.FC = () => {
-  const allSkills = [
-    {
-      heading: 'Languages',
-      skills: ['HTML・CSS', 'SCSS', 'JavaScript', 'TypeScript', 'PHP', 'Golang'],
-    },
-    {
-      heading: 'Technologies',
-      skills: ['React', 'Vue.js', 'Node.js', 'Laravel', 'WordPress', 'MySQL', 'Docker', 'AWS'],
-    },
-    {
-      heading: 'Editor',
-      skills: ['Neovim'],
-    },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
+  const [skillInfo, setSkillInfo] = useState<SkillInfo>({
+    title: '',
+    yearsOfExperience: '',
+    content: '',
+  });
+
+  const openSkillInModal = (skill: SkillInfo) => {
+    setSkillInfo({
+      ...skillInfo,
+      title: skill.title,
+      yearsOfExperience: skill.yearsOfExperience,
+      content: skill.content,
+    });
+    setIsOpen(true);
+  };
+
   return (
     <SectionRoot heading="Skills">
       {allSkills.map((item) => (
-        <div key={item.heading}>
-          <h3 className="text-xl font-bold">{item.heading}</h3>
-          <ul>
+        <div key={item.heading} className="mb-4">
+          <h3 className="text-xl font-bold mb-2">{item.heading}</h3>
+          <ul className="flex gap-4 flex-wrap">
             {item.skills.map((skill) => (
-              <li key={skill}>{skill}</li>
+              <li key={skill.title}>
+                <Button className="bg-blue-100 text-blue-900 hover:bg-blue-200" onClick={() => openSkillInModal(skill)}>
+                  {skill.title}
+                </Button>
+              </li>
             ))}
           </ul>
         </div>
       ))}
+      <Modal
+        title={skillInfo.title}
+        subTitle={skillInfo.yearsOfExperience}
+        content={skillInfo.content}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
     </SectionRoot>
   );
 };
