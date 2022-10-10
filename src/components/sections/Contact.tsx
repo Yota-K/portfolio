d5d42e4acf686322f59ab3fafa17941e7691e488
Y.K <powdersugar828828@gmail.com>
2022-10-10 21:03:46 +0900

    fix: Fixed Contact section text.

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,7 +5,6 @@ const Contact: React.FC = () => {
   return (
     <SectionRoot heading="Contact">
       <p>
-        仕事の依頼は
         <a
           className="text-blue-600 hover:underline"
           href="https://twitter.com/karukichi_yah"
