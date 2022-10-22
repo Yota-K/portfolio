2a061ec14e9777fc2614c5a07255f9a1da19460a
Y.K <powdersugar828828@gmail.com>
2022-10-22 14:16:40 +0900

    feat: Implemented change theme mode based on os settings.

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -1,34 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useDarkMode from 'use-dark-mode';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon, FaSun, FaTv } from 'react-icons/fa';
 import { Menu, Transition } from '@headlessui/react';
 
 const DropDownMenu: React.FC = () => {
+  const [mounted, setMounted] = useState(false);
   const darkMode = useDarkMode(false, {
     classNameDark: 'dark',
     classNameLight: 'light',
   });
-  const [isOsDarkmode, setIsOsDarkmode] = useState(false);
 
-  // Rendered Check
-  useEffect(() => {
-    if (typeof window !== undefined && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setIsOsDarkmode(true);
+  const toggleDarkmode = () => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      darkMode.enable();
+    } else {
+      darkMode.disable();
     }
-  }, []);
+  };
+
+  const localStorageKey = 'darkMode' as const;
+
+  const removeLocalStorageItemForDarkmode = () => {
+    // Avoidance "window is not available" error
+    const getDarkmode = window.localStorage.getItem(localStorageKey);
+
+    if (getDarkmode !== null) {
+      window.localStorage.removeItem(localStorageKey);
+      toggleDarkmode();
+    }
+  };
+
+  useEffect(() => {
+    if (!mounted) setMounted(true);
+
+    const getDarkmode = window.localStorage.getItem(localStorageKey);
+
+    if (getDarkmode === null) toggleDarkmode();
+  }, [darkMode]);
 
   return (
     <Menu as="div" className="relative inline-block text-left">
-      {isOsDarkmode && (
-        <Menu.Button
-          className={`p-2 rounded-full transition-all ${darkMode.value ? 'hover:bg-white/30' : 'hover:bg-black/10'}`}
-        >
+      <Menu.Button
+        className={`p-2 rounded-full transition-all ${darkMode.value ? 'hover:bg-white/30' : 'hover:bg-black/10'}`}
+      >
+        {mounted && (
           <>
             {darkMode.value && <FaMoon className="w-5 h-5 text-yellow-500" />}
             {!darkMode.value && <FaSun className="w-5 h-5 text-orange-500" />}
           </>
-        </Menu.Button>
-      )}
+        )}
+      </Menu.Button>
       <Transition
         enter="transition duration-100 ease-out"
         enterFrom="transform scale-95 opacity-0"
@@ -62,6 +83,19 @@ const DropDownMenu: React.FC = () => {
                 >
                   <FaMoon className="w-5 h-5 mr-2 text-yellow-500" />
                   Dark
+                </button>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  className={`${
+                    active ? 'bg-gray-100 dark:bg-violet-500' : 'text-gray-900'
+                  } group flex w-full items-center rounded-md px-2 py-2 text-sm dark:text-white`}
+                  onClick={removeLocalStorageItemForDarkmode}
+                >
+                  <FaTv className="w-5 h-5 mr-2 text-gray-500" />
+                  System
                 </button>
               )}
             </Menu.Item>
