fd895e29c026c8e12b431c4191de2682a309eee7
Y.K <powdersugar828828@gmail.com>
2022-12-22 22:04:57 +0900

    feat: Setup tailwind css.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Logo from '$lib/Logo.svelte';
+</script>
+
+<header
+	class="py-2 backdrop-blur bg-white/60 dark:bg-black/60 shadow-lg border-b fixed w-full z-40"
+>
+	<div class="flex justify-between items-center mx-auto w-11/12 lg:w-7/12">
+		<h1 class="inline-block lg:w-60 py-2">
+			<Logo />
+		</h1>
+		Dropdown
+	</div>
+</header>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	purge: false,
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
