248f19ce65d8169f0dd7eda9f34964990d06a126
Y.K <powdersugar828828@gmail.com>
2022-12-24 20:08:09 +0900

    fix: Fixed footer text.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <footer class="p-4 text-center bg-amber-50 dark:bg-gray-900 dark:text-white">
 	<span>
-		© 2022〜{new Date().getFullYear()}
+		© {new Date().getFullYear()}
 		{title}
 	</span>
 </footer>
