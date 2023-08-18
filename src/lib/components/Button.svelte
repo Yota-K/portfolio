e4092fbf8aaf9935f36c14a2aeb9887f90d8ea03
Y.K <powdersugar828828@gmail.com>
2023-08-19 01:41:21 +0900

    feat: Added contact form ui.

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,11 +3,16 @@
 	// 型キャストを使って `class` プロパティを作る
 	let className: string;
 	export { className as class };
+
+	export let fontSize: string | null = null;
+	export let fontWeight: string | null = null;
 </script>
 
 <button
 	on:click
-	class={`rounded-md px-4 py-2 text-sm font-medium transition-all duration-300 ${className}`}
+	class={`rounded-md px-4 py-2 ${fontSize === null ? 'text-sm' : fontSize} ${
+		fontWeight === null ? 'font-medium' : fontWeight
+	} transition-all duration-300 ${className}`}
 >
 	<slot />
 </button>
--- src/lib/components/sections/Contact.svelte
+++ src/lib/components/sections/Contact.svelte
@@ -1,17 +1,56 @@
 <script lang="ts">
 	import SectionRoot from '$lib/components/sections/SectionRoot.svelte';
+	import Button from '$lib/components/Button.svelte';
 </script>
 
 <SectionRoot heading="Contact">
-	<p>
-		<a
-			class="text-blue-600 hover:underline"
-			href="https://twitter.com/karukichi_yah"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			TwitterのDM
-		</a>
-		までお願いします。
-	</p>
+	<p>お問い合わせは以下のフォームよりお願いします。</p>
+	<form class="mt-4" action="">
+		<div class="flex flex-col gap-4">
+			<div>
+				<label class="block text-gray-700 text-sm font-bold mb-2 dark:text-white" for="name"
+					>お名前</label
+				>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="name"
+					name="name"
+					type="text"
+					required
+				/>
+			</div>
+			<div>
+				<label class="block text-gray-700 text-sm font-bold mb-2 dark:text-white" for="email"
+					>メールアドレス</label
+				>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="email"
+					name="email"
+					type="email"
+					required
+				/>
+			</div>
+			<div>
+				<label class="block text-gray-700 text-sm font-bold mb-2 dark:text-white" for="content"
+					>お問い合わせ内容</label
+				>
+				<textarea
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
+					id="content"
+					name="content"
+					cols="30"
+					rows="10"
+					required
+				/>
+			</div>
+			<div class="text-center">
+				<Button
+					class="bg-blue-600 text-white lg:w-80 w-full"
+					fontSize="text-xl"
+					fontWeight="font-bold">送信</Button
+				>
+			</div>
+		</div>
+	</form>
 </SectionRoot>
